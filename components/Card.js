147202b1763ed2c853d413ace0b007fe1fed2791
seemakaur11 <raiseema248@gmail.com>
2023-09-25 20:55:24 +0530

    card flip

--- components/Card.js
+++ components/Card.js
@@ -1,38 +1,44 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import card1 from "../public/Images/card1.jpg";
 import card2 from "../public/Images/card2.jpg";
 import card3 from "../public/Images/card3.jpg";
 import Image from 'next/image';
+import { CARD } from '../utility/Card';
 
 const Card = () => {
   return (
-    // <div className={`relative w-64 h-80 bg-white shadow-lg rounded-lg transition-transform duration-500 ease-in-out transform ${flipped ? 'rotate-y-180' : ''}`} onClick={toggleFlip}>
-    //   <div className="absolute inset-0 w-full h-full bg-gray-200 p-4 front">
-    //     <div className="flex items-center justify-center h-full">
-    //       <div className="text-center">
-    //         <img src={card1} alt="fgfgh" className="w-32 h-32 mx-auto mb-4 rounded-full" />
-    //         <p className="text-lg font-semibold">seema dft vcbfdg</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="absolute inset-0 w-full h-full bg-blue-500 text-white p-4 back">
-    //     <div className="flex items-center justify-center h-full">
-    //       <div className="text-center">
-    //         <p className="text-lg font-semibold">seemayfs dfdiufer</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 py-28 px-28' >
-        <div>
-            <Image src={card1} className='w-30 h-80' alt="card1" width={350} height={300} />
+    <div className='lg:px-28 md:px-24 sm:px-16 xs:px-12 xxs:px-2 py-16' >
+      <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-6 pb-16'>
+      {CARD.map((data, index) => {
+          return (
+            <div key={index} className='group w-full h-72 [perspective:1000px]'>
+              <div className='relative h-full w-full transition-all duration-700 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]'>
+                <div className='absolute inset-0'>
+                  <Image src={data.img} className='w-full h-72 object-cover shadow-xl shadow-black/40' alt="card1" width={350} height={350} />
+                </div>
+                <div className='absolute inset-0 h-full w-full bg-black/80 px-12 text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]'>
+                  <div className='text-start py-5 font-semibold'>
+                    <h1 className='text-2xl'>{data.title}</h1>
+                    <p className='font-[400] text-base mt-2'>{data.text}</p>
+                    <button className='mt-4 bg-amber-500 font-normal py-2 px-3 text-sm hover:bg-neutral-900'>VIEW DETAILS</button>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          )
+        })}
+        </div>
+
+
+        {/* <div>
+          <Image src={card2} className='w-full h-72' alt="card2" width={350} height={350} />
         </div>
         <div>
-            <Image src={card2} className='w-30 h-80' alt="card2" width={350} height={300} />
-        </div>
-        <div>
-            <Image src={card3} className='w-30 h-80' alt="card3" width={350} height={300} />
-        </div>
+          <Image src={card3} className='w-full h-72' alt="card3" width={350} height={350} />
+        </div> */}
+    
+
     </div>
   )
 }
---
+++ utility/card.js
@@ -0,0 +1,24 @@
+import card1 from "../public/Images/card1.jpg";
+import card2 from "../public/Images/card2.jpg";
+import card3 from "../public/Images/card3.jpg";
+
+export const CARD = [
+    {
+        id: 1,
+        img: card1,
+        title: "Careful storage of your goods",
+        text: "We offer a door-to-door logistics service to our customers by storing your goods very carefully."
+    },
+    {
+        id: 2,
+        img: card2,
+        title: "Quick & safe delivery of your goods",
+        text: "We offer quick and safe delivery in India at very affordable and genuine rates."
+    },
+    {
+        id: 3,
+        img: card3,
+        title: "Complex logistics",
+        text: "We have a systematic approach to the arrangement of life cycle of goods and related activities."
+    }
+]
